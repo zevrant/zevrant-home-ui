4eace8177d47697676df22da55be88d7b02758fd
Gereth Dittrick <gjdittrick@psi-it.com>
2019-12-10 13:13:09 -0500

    added discovery server config

--- src/main/java/net/zevrantservices/home/ui/zevranthomeui/ZevrantHomeUiApplication.java
+++ src/main/java/net/zevrantservices/home/ui/zevranthomeui/ZevrantHomeUiApplication.java
@@ -2,6 +2,7 @@ package net.zevrantservices.home.ui.zevranthomeui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ZevrantHomeUiApplication {
