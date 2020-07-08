2ca2b73d1d7fd6c3c83ad150cd928a84c9c61973
gdittrick <gjdittrick@pyramidsystems.com>
2020-07-08 17:52:32 -0400

    set mimetype for index.html

---
+++ src/main/java/net/zevrantservices/home/ui/zevranthomeui/config/MimeConfig.java
@@ -0,0 +1,16 @@
+package net.zevrantservices.home.ui.zevranthomeui.config;
+
+import org.springframework.boot.web.server.MimeMappings;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MimeConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+  @Override
+  public void customize(ConfigurableServletWebServerFactory factory) {
+    MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+    mappings.add("html", "text/javascript");
+    factory.setMimeMappings(mappings);
+  }
+}
