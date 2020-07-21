7dfb07b403a69ea2e14193e3007bcfd3dd6665c0
gdittrick <gjdittrick@pyramidsystems.com>
2020-07-20 22:41:02 -0400

    updated to conform to oauth service changes

--- src/main/java/net/zevrantservices/home/ui/zevranthomeui/controller/ForwardingController.java
+++ src/main/java/net/zevrantservices/home/ui/zevranthomeui/controller/ForwardingController.java
@@ -1,7 +1,10 @@
 package net.zevrantservices.home.ui.zevranthomeui.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class ForwardingController {
@@ -9,5 +12,11 @@ public class ForwardingController {
   @RequestMapping("/{path:[^\\.]+}/**")
   public String forward() {
     return "forward:/";
+  }
+
+  @RequestMapping("main/angular/favicon.ico")
+  public byte[] getFavIcon() throws IOException {
+    ClassPathResource resource = new ClassPathResource("favicon.ico");
+    return resource.getInputStream().readAllBytes();
   }
 }
